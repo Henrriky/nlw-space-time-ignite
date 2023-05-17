3ac2fcd8dcb6a293fc6b03f0a8f17fa7d080b2bb
Henrriky <tectutors.123@gmail.com>
2023-05-17 14:17:43 -0300

    import colors from figma to tailwind config

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,9 +2,8 @@ import Button from "@/components/Button";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
-      <h1 className="text-4xl font-bold">Oi 👏</h1>
-      <h1 className="text-4xl font-bold font-alt">Oi 👏</h1>
-    </div>
+    <main>
+       
+    </main>
   )
 }
