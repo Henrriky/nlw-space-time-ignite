cbf1d05d9d6ab22a4887fff9a5b6b5d96198a55b
Henrriky <tectutors.123@gmail.com>
2023-05-17 15:33:02 -0300

    left side profile style

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Button from "@/components/Button";
+import { User } from 'lucide-react'
 
 export default function Home() {
   return (
@@ -9,6 +10,16 @@ export default function Home() {
           <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 rounded-full blur-full "></div>
           {/* Stripes */}
           <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes"/>
+          {/* Profile/Sign-in */}
+          <a href="" className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors ">
+              <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+                <User className="w-5 h-5 text-gray-500"/>
+              </div>
+
+              <p className="text-sm leading-snug max-w-[140px]">
+                <span className="underline">Crie sua conta</span> e salve suas memórias
+              </p>
+          </a>
        </section>
 
        {/* Right */}
