108ff68d1938e4cb31f6fcf96f1c262579c56eb1
Henrriky <tectutors.123@gmail.com>
2023-05-17 15:08:45 -0300

    stars background

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* Left */}
-       <section className="relative flex flex-col items-start py-16 px-28 justify-between overflow-hidden border-r border-white/10">
+       <section className="relative flex flex-col items-start py-16 px-28 justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
           {/* Blur */}
           <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 rounded-full blur-full "></div>
           {/* Stripes */}
@@ -12,7 +12,7 @@ export default function Home() {
        </section>
 
        {/* Right */}
-       <section className="flex flex-col p-16">
+       <section className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
           <div className="flex flex-1 items-center justify-center">
             <p className="text-center leading-relaxed w-[360px]">
               Você ainda não registrou nenhuma lembrança, comece a {' '} 
