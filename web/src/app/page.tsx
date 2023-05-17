eb1f99b2c534634b672b0b96aa637de92b4fda70
Henrriky <tectutors.123@gmail.com>
2023-05-17 14:40:07 -0300

    left side blur

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* Left */}
-       <section></section>
+       <section className="relative flex flex-col items-start py-16 px-28 justify-between overflow-hidden">
+          <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 rounded-full blur-full "></div>
+       </section>
 
        {/* Right */}
        <section className="flex flex-col p-16">
