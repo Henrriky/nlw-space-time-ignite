7a251a9d64241f10a28602bcddd0489794744a3c
Henrriky <tectutors.123@gmail.com>
2023-05-17 14:00:46 -0300

    font configs

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
       <h1 className="text-4xl font-bold">Oi 👏</h1>
+      <h1 className="text-4xl font-bold font-alt">Oi 👏</h1>
     </div>
   )
 }
