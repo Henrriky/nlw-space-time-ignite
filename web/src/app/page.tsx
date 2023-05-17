6a851ee315bcab966a618e74d5afafc0c8440570
Henrriky <tectutors.123@gmail.com>
2023-05-17 15:04:06 -0300

    left stripes style

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,8 +4,11 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* Left */}
-       <section className="relative flex flex-col items-start py-16 px-28 justify-between overflow-hidden">
+       <section className="relative flex flex-col items-start py-16 px-28 justify-between overflow-hidden border-r border-white/10">
+          {/* Blur */}
           <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 rounded-full blur-full "></div>
+          {/* Stripes */}
+          <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes"/>
        </section>
 
        {/* Right */}
