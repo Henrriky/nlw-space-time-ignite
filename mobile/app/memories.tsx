1a95e071f7930bd02b42e3b028b8bd75be1748c3
Henrriky <tectutors.123@gmail.com>
2023-05-21 17:47:49 -0300

    Redirect screen

---
+++ mobile/app/memories.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from 'react-native'
+export default function Memories() {
+    return (
+        <View className="flex-1 justify-center item-center">
+            <Text>Memories</Text>
+        </View>
+    )
+}
