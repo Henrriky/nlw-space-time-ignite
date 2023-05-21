8d5016f2cf2ad15037d59f105aa43786587f0151
Henrriky <tectutors.123@gmail.com>
2023-05-21 17:17:50 -0300

    Add Expo AuthSession and return code in app.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,21 +8,51 @@ import Stripes from './src/assets/stripes.svg';
 import Logo from './src/assets/nlw-spacetime-logo.svg';
 
 import { styled } from 'nativewind';
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
+import { useEffect } from 'react';
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint: 'https://github.com/settings/connections/applications/5f12a2c88b4762c41881',
+};
 
 
 const StyledStripes = styled(Stripes);
 
 export default function App() {
 
+  const [request, response, signInWithGithub] = useAuthRequest(
+    {
+      clientId: '5f12a2c88b4762c41881',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime'
+      }),
+    },
+    discovery
+  );
+
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   });
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+
+      console.log(code);
+    }
+  }, [response]);
+
   
   if (!hasLoadedFonts) {
     return null;
   }
+
+  
 
   return (
     <ImageBackground 
@@ -37,7 +67,10 @@ export default function App() {
           <Text className="text-gray-50 font-title text-2xl text-center leading-tight">Sua cápsula do tempo</Text>
           <Text className="text-gray-100 font-body text-base text-center leading-relaxed">Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!</Text>
         </View>
-        <TouchableOpacity className="bg-green-500 px-5 py-3 rounded-full" activeOpacity={0.7}>
+        <TouchableOpacity 
+          className="bg-green-500 px-5 py-3 rounded-full" 
+          activeOpacity={0.7} 
+          onPress={() => signInWithGithub()}>
           <Text className="font-alt text-sm text-black ">COMEÇAR A CADASTRAR</Text>
         </TouchableOpacity>
       </View>
